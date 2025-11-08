1a1bace2cba9aa8f51c22b6710d05d5b91c5f8bb
vasuhere34 <vasuhere34@gmail.com>
2025-11-08 21:20:54 +0530

    gd

--- amazon/settings.py
+++ amazon/settings.py
@@ -59,9 +59,9 @@ MIDDLEWARE = [
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-   "http://localhost:5173",
-]
+# CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:5173",
+# ]
 ROOT_URLCONF = 'amazon.urls'
 
 TEMPLATES = [
