47bd0c9fccefca5f3ed7148bfda87ec90a8eaba4
vasuhere34 <vasuhere34@gmail.com>
2025-11-08 20:14:29 +0530

    fd

--- amazon/settings.py
+++ amazon/settings.py
@@ -45,10 +45,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
         'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,10 +57,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://vasu-ecommerce-site.netlify.app",
+   "http://localhost:5173",
 ]
 ROOT_URLCONF = 'amazon.urls'
 
